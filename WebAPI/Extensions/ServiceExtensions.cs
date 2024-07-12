fde3fc0463c942eb959e131272e108a46b44c1e9
venudamarla15 <66801216+venudamarla15@users.noreply.github.com>
2024-07-12 14:48:56 -0500

    Custom services

---
+++ WebAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("Corspolicy", builder =>
+                  builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+            });
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+             services.Configure<IISOptions>(options => 
+             {
+                 
+             });
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,14 +1,32 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using WebAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
