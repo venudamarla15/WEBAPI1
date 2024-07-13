c3a9d68d532e5b9c3ee9f35d0f45ecedd114d96d
venudamarla15 <66801216+venudamarla15@users.noreply.github.com>
2024-07-12 20:27:48 -0500

    middlewar

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -30,6 +30,47 @@ app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
+app.Use(async( context, next)=>{
+    Console.WriteLine($"Logic befor the nwxt delegate in the Use Method");
+    await next.Invoke(context);
+    Console.WriteLine($"Logic After the nwxt delegate in the Use Method");
+});
+
+app.Map("/usingmapbranch", builder =>
+{
+    app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder =>
+    {
+        builder.Run(async context =>
+        {
+            await context.Response.WriteAsync("hello from mapwhen branch");
+        });
+    });
+
+    app.Run(async context =>
+    {
+
+    });
+    builder.Use(async (HttpContext context, RequestDelegate next) =>
+    {
+        Console.WriteLine("Map branch logic in the Use method before the nextdelegate");
+        await next.Invoke(context);
+        Console.WriteLine("Map branch logic in the Use method after the nextdelegate");
+
+    });
+    builder.Run(async context =>
+    {
+        Console.WriteLine($"Map branch response to the client in the Run method");
+        await context.Response.WriteAsync("Hello from the map branch.");
+
+    });
+});
+
+app.Run(async context =>
+{
+    Console.WriteLine($"Writing the response to the client in the Run method");
+    await context.Response.WriteAsync("Hello from the middleware");
+});
+
 app.MapControllers();
 
 app.Run();
